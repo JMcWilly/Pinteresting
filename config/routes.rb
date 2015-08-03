6a1ac6ac7dbc14a68de6dd197208b24266897a08
Jason McWilliams <jason.mcwilliams00@yahoo.com>
2015-08-02 23:40:33 -0400

    added learning page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" # creates about_path
+  get "learning" => "pages#learning" # creates learning_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
